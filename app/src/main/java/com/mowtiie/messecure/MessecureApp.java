fe74b03006b6fe8169b9e276ddbf0bb49bcad395
mowtiie <mowtiie.dev@gmail.com>
2026-05-28 11:22:26 +0800

    Updated App class

--- app/src/main/java/com/mowtiie/messecure/MessecureApp.java
+++ app/src/main/java/com/mowtiie/messecure/MessecureApp.java
@@ -2,6 +2,7 @@ package com.mowtiie.messecure;
 
 import android.app.Application;
 
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.PersistentCacheSettings;
@@ -9,22 +10,28 @@ import com.google.firebase.functions.FirebaseFunctions;
 import com.mowtiie.messecure.util.KeystoreHelper;
 
 public class MessecureApp extends Application {
+
+    private static final boolean USE_EMULATOR = true;
+    private static final String EMULATOR_HOST = "192.168.1.69";
+
     @Override
     public void onCreate() {
         super.onCreate();
 
-        FirebaseFunctions.getInstance().useEmulator("192.168.1.69", 5001);
-        FirebaseFunctions.getInstance().useEmulator("192.168.1.69", 8080);
-
-        PersistentCacheSettings cacheSettings = PersistentCacheSettings.newBuilder()
-                .setSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED)
-                .build();
-
-        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
-                .setLocalCacheSettings(cacheSettings)
-                .build();
-
-        FirebaseFirestore.getInstance().setFirestoreSettings(settings);
+        if (USE_EMULATOR) {
+            FirebaseAuth.getInstance().useEmulator(EMULATOR_HOST, 9099);
+            FirebaseFirestore.getInstance().useEmulator(EMULATOR_HOST, 8080);
+            FirebaseFunctions.getInstance().useEmulator(EMULATOR_HOST, 5001);
+        } else {
+            FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                    .setLocalCacheSettings(
+                            PersistentCacheSettings.newBuilder()
+                                    .setSizeBytes(FirebaseFirestoreSettings.CACHE_SIZE_UNLIMITED)
+                                    .build()
+                    )
+                    .build();
+            FirebaseFirestore.getInstance().setFirestoreSettings(settings);
+        }
 
         try {
             KeystoreHelper.generateKey();
