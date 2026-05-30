e70891eb7b17138ad7c401c3a6f62ecf07ccfb50
mowtiie <mowtiie.dev@gmail.com>
2026-05-30 11:25:21 +0800

    Added app icon

--- app/src/main/java/com/mowtiie/messecure/ui/activities/RegisterActivity.java
+++ app/src/main/java/com/mowtiie/messecure/ui/activities/RegisterActivity.java
@@ -27,7 +27,7 @@ import java.util.Map;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    private static final String ALLOWED_DOMAIN = "@gmail.com";
+    private static final String ALLOWED_DOMAIN = "@sti.edu.ph";
 
     private TextInputEditText nameField, emailField, passwordField, confirmPasswordField;
     private Button registerButton;
@@ -55,8 +55,6 @@ public class RegisterActivity extends AppCompatActivity {
         confirmPasswordField = findViewById(R.id.confirmPasswordField);
         registerButton       = findViewById(R.id.registerButton);
         progressBar          = findViewById(R.id.progressBar);
-
-        emailField.setHint(ALLOWED_DOMAIN);
 
         registerButton.setOnClickListener(v -> attemptRegister());
 
